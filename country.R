8da3aa7ba480426f62ec722ff0383c32c0b69c50
ginobili0 <yaswateja@gmail.com>
2015-05-15 12:20:30 -0400

    changed color scheme

--- country.R
+++ country.R
@@ -7,10 +7,10 @@ countries$GDP = as.numeric(as.character(countries$GDP))
 
 sPDF = joinCountryData2Map(countries, joinCode = "NAME", nameJoinColumn = "Country",
                            verbose = TRUE)
+
+png(filename = "CountryGDP.png", width = 800, height = 600, units = "px")
 mapParams = mapCountryData(sPDF, nameColumnToPlot = "GDP", missingCountryCol = "dark grey",
-                           addLegend = FALSE, oceanCol = "lightsteelblue2")
+                           addLegend = FALSE, oceanCol = "lightsteelblue2", numCats = 15,
+                           catMethod = "logFixedWidth", colourPalette = brewer.pal(9, "RdPu"))
 do.call(addMapLegend, c(mapParams, legendWidth = 0.5, legendMar = 2))
-
-
-
-
+dev.off()
